46c536f755cfc7444f01045b98e56f98ed6fa400
Lucio Anderlini <Lucio.Anderlini@cern.ch>
2024-04-28 19:29:11 +0200

    Attempt of serving custom assets

--- aiinfn_dash/board_vkd/callbacks.py
+++ aiinfn_dash/board_vkd/callbacks.py
@@ -6,10 +6,10 @@ from aiinfn_dash.auth import User
 
 def register_callbacks(app):
     @app.callback(
-        dash.Output('vkd:title', 'children'),
-        dash.Input('vkd:my-input', 'value'),
+        dash.Output('user-name', 'children'),
+        dash.Input('status-table', 'value'),
     )
     def vkd_update_title(my_input):
         logging.info("Called vkd update_title")
         with User.from_session() as user:
-            return my_input.replace("%", f" {user.name} ") if '%' in my_input else f"{my_input} {user.name}"
+            return user.name
--- aiinfn_dash/board_vkd/layout.py
+++ aiinfn_dash/board_vkd/layout.py
@@ -4,7 +4,26 @@ from dash import html, dcc, dash_table
 def layout():
     cols = ["A", "B", "C"]
     return html.Div([
-        html.H1(f'Ciao, !', id='vkd:title'),
-        dcc.Input(id='vkd:my-input', value='', type='text'),
-        dash_table.DataTable(data=[], columns=[{'name': c, 'id': c} for c in cols])
+            html.Div([
+                html.Span("👤", className='user-logo'),
+                html.Span(f"Username", className='user-name', id='user-name'),
+            ], className="user-menu"),
+            html.Div([
+                html.H1(f'VK Dispatcher Dashboard', id='vkd:title', className='main-title'),
+                dash_table.DataTable(data=[], columns=[{'name': c, 'id': c} for c in cols], id='status-table')
+        ], className='data-table'),
+        html.Div(
+            [
+                html.Span(
+                    f"©️ AI_INFN 2024. Contribute to this dashboard on GitHub: ",
+                    className='footer-message',
+                ),
+                html.A(
+                    f"landerlini/ai-infn-dashboard",
+                    href="https://github.com/landerlini/ai-infn-dashboard",
+                    target='_blank',
+                    className='footer-message',
+                ),
+            ], className='footer',
+        )
     ])
